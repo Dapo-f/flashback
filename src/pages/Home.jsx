65f89787809f7b25a3abe82dfabee103dd0f9732
Adedapo Fagorala <adedapofagorala@gmail.com>
2026-05-06 09:59:02 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../Components/Header"
+import HeroSection from "../Components/HeroSection"
+function Home() {
+    return(
+        <>
+            <Header/>
+            <HeroSection/>
+            <h1 className="text-3xl text-center font-bold">Home</h1>
+            <p>Lorem Hello World</p>
+        </>
+    )
+}
+
+export default Home
