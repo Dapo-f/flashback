1483ab0319c303ac65a5fe9b1b9c808298999458
Adedapo Fagorala <adedapofagorala@gmail.com>
2026-05-06 14:21:06 +0100

    Added routing to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react"
-import "./App.css"
-import Home from "./pages/Home"
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Error404 from "./pages/Error404";
 
 class App extends React.Component {
   render() {
     return (
-      <>
-       <Home/>
-      </>
-    )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<Error404/>}/>
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../Components/Header";
+
+function About() {
+  return (
+    <>
+      <Header />
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius dolor
+        illo accusantium dolorem mollitia, atque debitis velit quos amet
+        praesentium hic placeat aliquid distinctio, ullam similique ipsa eveniet
+        laborum ad?
+      </p>
+    </>
+  );
+}
+
+export default About;
